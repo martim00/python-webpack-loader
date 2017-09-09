7a229a1abe411a02251d9826eb177b3716933026
Martim <martim00@gmail.com>
2017-09-08 23:52:12 -0300

    fix workflow. now does not save a tmp py file since it is already on disk. Also improve error messages

--- index.js
+++ index.js
@@ -1,36 +1,30 @@
-
 const cmd = require('node-cmd')
 const fs = require('fs');
 const path = require('path');
 
-function findEntry(mod) {
-    if (mod.reasons.length > 0 && mod.reasons[0].module.resource) {
-        return findEntry(mod.reasons[0].module)
-    }
-    return mod.resource;
-}
-
 module.exports = function (source) {
-
-//    console.log(source);
-    var entry = findEntry(this._module)
-    var name = path.basename(entry, ".py");
-    console.log("entry " + name)
 
     var callback = this.async();
 
-    fs.writeFile(name + ".py", source, function(err) {
+//    console.log(source);
+    var entry = this._module.resource;
+
+    var name = path.basename(entry, ".py");
+    console.log("entry " + name)
+
+    if (!entry.toLowerCase().endsWith(".py")) {
+        console.log("This loader only handles .py files. This could be a problem with your webpack.config.js file. Please add a rule for .py files in your modules entry.");
+        callback(null, source);
+    }
+
+    cmd.get('transcrypt -b -n ' + entry, function(err, data, stderr) {
         if (!err) {
-
-            cmd.get('transcrypt -b -n ' + name + '.py', function(err, data, stderr) {
-                if (!err) {
-                    js = fs.readFileSync("./__javascript__/" + name + ".js", "utf8")
-                    callback(null, js);
-
-                }
-
-            });
-
+            js = fs.readFileSync("./__javascript__/" + name + ".js", "utf8")
+            callback(null, js);
+        }
+        else {
+            console.log("Some error occurred on Transcrypt compiler execution. Did you have installed Transcrypt? If no, please run `pip install transcrypt`");
+            console.log("Error: " + err);
         }
 
     });
